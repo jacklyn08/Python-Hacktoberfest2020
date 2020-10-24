2c3f67fdfb4e4f9c116b4dd8c14960f830cdd870
jacklyn08 <71274319+jacklyn08@users.noreply.github.com>
2020-10-24 10:56:37 +0200

    Added arguments
    I added arguments to the program so the user can call the program from terminal with arguments without calling the inputs

--- python/DNS_Lookup.py
+++ python/DNS_Lookup.py
@@ -3,15 +3,23 @@ i)Program to find IP address from Domain Name.
 ii)Program to find Server Name from IP address
 '''
 import socket
+import sys
 
 print ('Welcome to DNS to IP Address')
-URL=input('Enter URL: ')
+URL=sys.argv[1]
 
 addr1 = socket.gethostbyname(URL)
 
 print(addr1)
 print ('WelCome IP address to DNS')
-IP=input('Enter IP Address: ')
+IP= sys.argv[2]
 addr6=socket.gethostbyaddr(IP)
 print (addr6)
 
+if len(URL) <= 0:
+    print("[+]insert the url[+]")
+    sys.exit()
+
+if len(IP) <= 0:
+    print("[+]insert the IP[*]")
+    sys.exit()
